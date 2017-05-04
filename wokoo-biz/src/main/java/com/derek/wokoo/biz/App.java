c95d8e252672be667fa3041141f014c2cb924fd1
Derek Wu <derek.wu@aliyun.com>
2017-05-05 00:07:25 +0800

    INIT project

---
+++ wokoo-biz/src/main/java/com/derek/wokoo/biz/App.java
@@ -0,0 +1,7 @@
+package com.derek.wokoo.biz;
+
+/**
+ * Created by derek.wu on 2017-05-05.
+ */
+public class App {
+}
---
+++ wokoo-dal/src/main/java/com/derek/wokoo/dal/App.java
@@ -0,0 +1,7 @@
+package com.derek.wokoo.dal;
+
+/**
+ * Created by derek.wu on 2017-05-05.
+ */
+public class App {
+}
